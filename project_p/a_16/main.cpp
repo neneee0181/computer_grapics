9ef136b21da73b7d679bffd3c95827e2a17f662f
neneee0181 <2022180024@tukorea.ac.kr>
2024-10-21 20:38:41 +0900

    feat : ppt 추가

--- project_p/a_16/main.cpp
+++ project_p/a_16/main.cpp
@@ -392,7 +392,7 @@ int main(int argc, char** argv) {
         cout << "GLEW Initialized\n";  // 초기화 성공 시 메시지 출력
 
     make_shaderProgram();  // 쉐이더 프로그램 생성
-        
+
     // 모델들을 로드하고 벡터에 추가
     Model modelSqu, modelSphere;
 
