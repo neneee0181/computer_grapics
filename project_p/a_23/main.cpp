f535abc6aa8bea637950b9132dcbb61ed2e6e654
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-21 01:26:09 +0900

    1

--- project_p/a_23/main.cpp
+++ project_p/a_23/main.cpp
@@ -9,8 +9,8 @@
 #include <unordered_map>
 #include <random>
 
-#include "LoadObj.h"
 #include "shaderMaker.h"
+#include "LoadWall.h"
 
 using namespace std;
 
@@ -18,19 +18,15 @@ void InitBuffer();
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 
-vector<Model> models;
-vector<GLuint> vaos;
-vector<vector<GLuint>> vbos;
-
+//카메라
 glm::vec3 cameraPos = glm::vec3(0.0, 0.0, 100);
 glm::vec3 cameraDirection = glm::vec3(0.0, 0.0, 0.0);
 glm::vec3 cameraUp = glm::vec3(0.0, 1.0, 0.0);
 glm::mat4 projection = glm::mat4(1.0f);
 glm::mat4 view = glm::mat4(1.0f);
 
-Model model_box, model_body;
-
-std::unordered_map<char, bool> keyState;
+//키
+unordered_map<char, bool> keyState;
 
 void keyDown_s(const char& key) {
     keyState[key] = true;
@@ -103,7 +99,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(width, height);
-    glutCreateWindow("template");
+    glutCreateWindow("23번실습");
 
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
@@ -115,25 +111,7 @@ int main(int argc, char** argv) {
 
     make_shaderProgram();
 
-    read_obj_file("obj/box.obj", model_box, "box");
-    read_obj_file("obj/body.obj", model_body, "body");
-
-    glm::mat4 matrix_box = glm::mat4(1.0f);
-    matrix_box = glm::translate(matrix_box, glm::vec3(0.0, 0.0, 0.0));
-    model_box.modelMatrix = matrix_box * model_box.modelMatrix;
-
-    models.push_back(model_box);
-    models.push_back(model_body);
-
-    for (auto& model : models) {
-        if (!model.material.map_Kd.empty()) {
-            model.material.textureID = loadTexture(model.material.map_Kd);
-            model.material.hasTexture = true; // 텍스처가 있음을 표시
-        }
-        else {
-            model.material.hasTexture = false;
-        }
-    }
+    load_wall_obj();
 
     InitBuffer();
 
@@ -178,52 +156,8 @@ GLvoid drawScene() {
     glUniform3fv(lightPosLoc, 1, glm::value_ptr(-glm::vec3(900.0, -900.0, -900.0)));
     glUniform3fv(lightColorLoc, 1, glm::value_ptr(glm::vec3(0.6f, 0.65f, 0.6f)));
 
-    glEnable(GL_DEPTH_TEST);
+    draw_wall(shaderProgramID, isKeyPressed_s);
 
-    GLint modelStatus = glGetUniformLocation(shaderProgramID, "modelStatus");
-    GLint modelLoc = glGetUniformLocation(shaderProgramID, "model");
-
-    for (size_t i = 0; i < models.size(); ++i) {
-
-        if (models[i].model_status) {
-            glBindVertexArray(vaos[i]);
-            glLineWidth(1.0f);
-            if (models[i].material.hasTexture) {
-                glActiveTexture(GL_TEXTURE0);
-                glBindTexture(GL_TEXTURE_2D, models[i].material.textureID);
-                glUniform1i(glGetUniformLocation(shaderProgramID, "texture1"), 0);
-                glUniform1i(glGetUniformLocation(shaderProgramID, "hasTexture"), 1);
-            }
-            else {
-                glUniform1i(glGetUniformLocation(shaderProgramID, "hasTexture"), 0);
-
-                GLint KaLoc = glGetUniformLocation(shaderProgramID, "Ka");
-                GLint KdLoc = glGetUniformLocation(shaderProgramID, "Kd");
-                GLint KsLoc = glGetUniformLocation(shaderProgramID, "Ks");
-                GLint NsLoc = glGetUniformLocation(shaderProgramID, "Ns");
-
-                glUniform3fv(KaLoc, 1, glm::value_ptr(models[i].material.Ka));
-                glUniform3fv(KdLoc, 1, glm::value_ptr(models[i].material.Kd));
-                glUniform3fv(KsLoc, 1, glm::value_ptr(models[i].material.Ks));
-                glUniform1f(NsLoc, models[i].material.Ns);
-            }
-
-            if (models[i].name == "box" || models[i].name == "body") {
-                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(models[i].modelMatrix));
-                glUniform1i(modelStatus, 0);
-                if (isKeyPressed_s('1'))
-                    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-                else
-                    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-                glDrawElements(GL_TRIANGLES, models[i].faces.size() * 3, GL_UNSIGNED_INT, 0);
-            }
-
-            glBindVertexArray(0);
-        }
-    }
-
-
-    glDisable(GL_DEPTH_TEST);
     glutSwapBuffers();
     GLenum err;
     while ((err = glGetError()) != GL_NO_ERROR) {
@@ -234,98 +168,53 @@ GLvoid drawScene() {
 // 버퍼 초기화 함수
 void InitBuffer() {
     //-----------------------------------------------------------------------------------------------------------
-    vaos.resize(models.size());
-    vbos.resize(models.size(), vector<GLuint>(4)); // 모델마다 4개의 VBO가 필요 (정점, 색상, 법선, 텍스처 좌표)
-
-    for (size_t i = 0; i < models.size(); ++i) {
-        glGenVertexArrays(1, &vaos[i]);
-        glBindVertexArray(vaos[i]);
-
-        // VBO 생성
-        glGenBuffers(4, vbos[i].data());
-
-        // 정점 버퍼 설정
-        glBindBuffer(GL_ARRAY_BUFFER, vbos[i][0]);
-        glBufferData(GL_ARRAY_BUFFER, models[i].vertices.size() * sizeof(glm::vec3), models[i].vertices.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 0에 정점 할당
-        glEnableVertexAttribArray(0);
-
-        // 색상 버퍼 설정
-        //glBindBuffer(GL_ARRAY_BUFFER, vbos[i][1]);
-        //glBufferData(GL_ARRAY_BUFFER, models[i].colors.size() * sizeof(glm::vec3), models[i].colors.data(), GL_STATIC_DRAW);
-        //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 1에 색상 할당
-        //glEnableVertexAttribArray(1);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbos[i][2]);  // 법선용 VBO
-        glBufferData(GL_ARRAY_BUFFER, models[i].normals.size() * sizeof(glm::vec3), models[i].normals.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 1에 법선 할당
-        glEnableVertexAttribArray(1);
-
-        // 텍스처 좌표 VBO 설정
-        glBindBuffer(GL_ARRAY_BUFFER, vbos[i][3]);  // 4번째 VBO가 텍스처 좌표용이라고 가정
-        glBufferData(GL_ARRAY_BUFFER, models[i].texCoords.size() * sizeof(glm::vec2), models[i].texCoords.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);  // location 2에 텍스처 좌표 할당
-        glEnableVertexAttribArray(2);
-
-        // 면 인덱스 데이터 (EBO) 설정
-        vector<unsigned int> indices;
-        for (const Face& face : models[i].faces) {
-            indices.push_back(face.v1);
-            indices.push_back(face.v2);
-            indices.push_back(face.v3);
-        }
-
-        GLuint ebo;
-        glGenBuffers(1, &ebo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-    }
+    initBuffer_wall();
 }
 
-void AddModelBuffer(const Model& model) {
-    GLuint vao;
-    vector<GLuint> vbo(4);
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    // VBO 생성
-    glGenBuffers(4, vbo.data());
-
-    // 정점 버퍼 설정
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glBufferData(GL_ARRAY_BUFFER, model.vertices.size() * sizeof(glm::vec3), model.vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 0에 정점 할당
-    glEnableVertexAttribArray(0);
-
-    // 법선 버퍼 설정
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-    glBufferData(GL_ARRAY_BUFFER, model.normals.size() * sizeof(glm::vec3), model.normals.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 1에 법선 할당
-    glEnableVertexAttribArray(1);
-
-    // 텍스처 좌표 버퍼 설정
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
-    glBufferData(GL_ARRAY_BUFFER, model.texCoords.size() * sizeof(glm::vec2), model.texCoords.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);  // location 2에 텍스처 좌표 할당
-    glEnableVertexAttribArray(2);
-
-    // 면 인덱스 데이터 (EBO) 설정
-    vector<unsigned int> indices;
-    for (const Face& face : model.faces) {
-        indices.push_back(face.v1);
-        indices.push_back(face.v2);
-        indices.push_back(face.v3);
-    }
-
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-
-    // 설정이 끝난 VAO와 VBO를 저장
-    vaos.push_back(vao);
-    vbos.push_back(vbo);
-
-    glBindVertexArray(0); // VAO unbind
-}
+//void AddModelBuffer(const Model& model) {
+//    GLuint vao;
+//    vector<GLuint> vbo(4);
+//
+//    glGenVertexArrays(1, &vao);
+//    glBindVertexArray(vao);
+//
+//    // VBO 생성
+//    glGenBuffers(4, vbo.data());
+//
+//    // 정점 버퍼 설정
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+//    glBufferData(GL_ARRAY_BUFFER, model.vertices.size() * sizeof(glm::vec3), model.vertices.data(), GL_STATIC_DRAW);
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 0에 정점 할당
+//    glEnableVertexAttribArray(0);
+//
+//    // 법선 버퍼 설정
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+//    glBufferData(GL_ARRAY_BUFFER, model.normals.size() * sizeof(glm::vec3), model.normals.data(), GL_STATIC_DRAW);
+//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);  // location 1에 법선 할당
+//    glEnableVertexAttribArray(1);
+//
+//    // 텍스처 좌표 버퍼 설정
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+//    glBufferData(GL_ARRAY_BUFFER, model.texCoords.size() * sizeof(glm::vec2), model.texCoords.data(), GL_STATIC_DRAW);
+//    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);  // location 2에 텍스처 좌표 할당
+//    glEnableVertexAttribArray(2);
+//
+//    // 면 인덱스 데이터 (EBO) 설정
+//    vector<unsigned int> indices;
+//    for (const Face& face : model.faces) {
+//        indices.push_back(face.v1);
+//        indices.push_back(face.v2);
+//        indices.push_back(face.v3);
+//    }
+//
+//    GLuint ebo;
+//    glGenBuffers(1, &ebo);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+//
+//    // 설정이 끝난 VAO와 VBO를 저장
+//    vaos.push_back(vao);
+//    vbos.push_back(vbo);
+//
+//    glBindVertexArray(0); // VAO unbind
+//}
