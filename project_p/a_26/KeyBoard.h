1f28114591cf7fdb1ad39733512731931121f3ba
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-23 21:59:23 +0900

    1

--- project_p/a_26/KeyBoard.h
+++ project_p/a_26/KeyBoard.h
@@ -42,6 +42,8 @@ std::mt19937 gen(rd());
 std::uniform_real_distribution<> dist(0.0, 1.0);
 
 void keyDown(unsigned char key, int x, int y) {
+    float cameraSpeed = 0.5f; // 카메라 이동 속도
+    float rotationSpeed = 2.0f; // 회전 속도
 
     keyDown_s(key);
 
@@ -50,6 +52,30 @@ void keyDown(unsigned char key, int x, int y) {
     case 'c':
         lightColor = glm::vec3(dist(gen), dist(gen), dist(gen));
         break;
+    case 'z':
+        cameraPos.z -= cameraSpeed;
+        break;
+    case 'Z':
+        cameraPos.z += cameraSpeed;
+        break;
+    case 'x':
+        cameraPos.x += cameraSpeed;
+        break;
+    case 'X':
+        cameraPos.x -= cameraSpeed;
+        break;
+    case 'r': {
+        // y축을 기준으로 양방향 회전
+        glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(rotationSpeed), glm::vec3(0.0f, 1.0f, 0.0f));
+        cameraPos = glm::vec3(rotationMatrix * glm::vec4(cameraPos, 1.0f));
+        break;
+    }
+    case 'R': {
+        // y축을 기준으로 음방향 회전
+        glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-rotationSpeed), glm::vec3(0.0f, 1.0f, 0.0f));
+        cameraPos = glm::vec3(rotationMatrix * glm::vec4(cameraPos, 1.0f));
+        break;
+    }
     case 'q':
         std::cout << " 프로그램 종료 " << std::endl;
         exit(0);
