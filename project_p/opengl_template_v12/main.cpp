3dac6090b102965b0a2e6d67e590ea3b5fd3a9f0
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-29 13:57:02 +0900

    1

--- project_p/opengl_template_v12/main.cpp
+++ project_p/opengl_template_v12/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
 
     initPhysics(); // Bullet 초기화 함수 호출
 
-    DefaultModel* default_model = new DefaultModel("obj/12140_Skull_v3_L2.obj", "straight_road", "sphere", glm::scale(glm::mat4(1.0f), glm::vec3(1.0, 1.0, 1.0))); // 실제 모델 가져오기
+    DefaultModel* default_model = new DefaultModel("obj/straight_road.obj", "straight_road", "sphere", glm::scale(glm::mat4(1.0f), glm::vec3(10.0, 10.0, 10.0))); // 실제 모델 가져오기
     models.push_back(default_model);
 
     // 디버깅 출력
